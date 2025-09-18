d316f0a327d4abed4df8e0b0864bf7cec31423da
BenjaminBaredes <bbaredes@gmail.com>
2025-09-18 12:07:22 -0300

    casi base

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ import {
   seleccionarSabores,
 } from "./dataEntry.js";
 
+import fs from 'fs';
+
 // Cargar sabores y productos
 // COMPLETEN USTEDES
-let sabores;
-let productos;
-
+let sabores = fs.readFileSync("data/sabores.json", "utf-8");
+let productos = fs.readFileSync("data/productos.json", "utf-8");
+let pedido = fs.readFileSync("data/pedidos.json", "utf-8");
 // Ingresar cliente
 let cliente = ingresarCliente();
 // Elegir producto
@@ -19,5 +21,14 @@ let gustos = seleccionarCantidadGustos(producto.maxGustos);
 // Elegir sabores
 let saboresElegidos = seleccionarSabores(sabores, gustos);
 
+let pedidofinal = JSON.parse(pedido);
+
+sabores.push(sabores);
+
+// 4) Convertir el array a JSON con identación (2 espacios)
+let nuevoJson = JSON.stringify(nombres, null, 2);
+
+// 5) Escribir el archivo actualizado
+fs.writeFileSync("nombres.json", nuevoJson);
 // Guardar pedido
 // COMPLETEN USTEDES
