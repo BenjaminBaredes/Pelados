7ee0aa4cc08944b5f83f2a839bd3c1a52ac881d8
BenjaminBaredes <bbaredes@gmail.com>
2025-09-25 12:06:41 -0300

    base

--- index.js
+++ index.js
@@ -4,31 +4,25 @@ import {
   seleccionarProducto,
   seleccionarSabores,
 } from "./dataEntry.js";
+import fs from "fs";
 
-import fs from 'fs';
+let sabores = JSON.parse (fs.readFileSync("data/sabores.json", "utf-8"));
+let productos = JSON.parse (fs.readFileSync("data/productos.json", "utf-8"));;
 
-// Cargar sabores y productos
-// COMPLETEN USTEDES
-let sabores = fs.readFileSync("data/sabores.json", "utf-8");
-let productos = fs.readFileSync("data/productos.json", "utf-8");
-let pedido = fs.readFileSync("data/pedidos.json", "utf-8");
-// Ingresar cliente
+
 let cliente = ingresarCliente();
-// Elegir producto
+
 let producto = seleccionarProducto(productos);
-// Elegir cantidad de gustos
+
 let gustos = seleccionarCantidadGustos(producto.maxGustos);
-// Elegir sabores
+
 let saboresElegidos = seleccionarSabores(sabores, gustos);
 
-let pedidofinal = JSON.parse(pedido);
+let pedidos = JSON.parse (fs.readFileSync("data/pedidos.json", "utf-8"));
+pedidos.push (cliente)
+pedidos.push (producto)
+pedidos.push (gustos)
+pedidos.push (saboresElegidos)
 
-sabores.push(sabores);
-
-// 4) Convertir el array a JSON con identación (2 espacios)
-let nuevoJson = JSON.stringify(nombres, null, 2);
-
-// 5) Escribir el archivo actualizado
-fs.writeFileSync("nombres.json", nuevoJson);
-// Guardar pedido
-// COMPLETEN USTEDES
+let nuevoPedido = JSON.stringify (pedidos);
+fs.writeFileSync("data/pedidos.json", nuevoPedido)
